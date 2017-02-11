c67dd14bc8dd33278645066e2f4e6e6fd7f8e38d
Eugene Isupov <workline521@gmail.com>
2017-02-11 10:25:10 +0400

    Minimize JavaScript

--- js/catalog.js
+++ js/catalog.js
@@ -6,11 +6,25 @@ for (var i = 0; i < buy.length; i++) {
     buy[i].addEventListener('click', function (event) {
         event.preventDefault();
         cbuyform.classList.add("show-buy-form");
+		cbuyform.classList.remove("fadeout");
                 });
 }
 cexitbuyform.addEventListener("click", function(event) {
         event.preventDefault();
 	    cbuyform.classList.remove("show-buy-form");
-	          
+	    cbuyform.classList.add("fadeout");     
               });
 
+function closeCbuy() {
+	cbuyform.classList.remove("fadeout");
+}
+cbuyform.addEventListener("animationend", closeCbuy, false);
+
+window.addEventListener("keydown", function(event) {
+	if ( event.keyCode === 27){
+		if(cbuyform.classList.contains("show-buy-form"
+		)){
+		cbuyform.classList.remove("show-buy-form");
+	}
+  }
+});
--- js/index.js
+++ js/index.js
@@ -1,20 +1,26 @@
 "use strict";
+var i;
 var showmap = document.querySelector(".map");
 var popup = document.querySelector(".modal-content-map");
 var close = popup.querySelector(".modal-content-close");
+
 var mail = document.querySelector(".lost");
 var mailform = document.querySelector(".modal-mail");
 var closemail = mailform.querySelector(".modal-content-close");
+
 var buy = document.querySelectorAll(".buy");
 var buyform = document.querySelector(".modal-content");
 var exitbuyform = buyform.querySelector(".modal-content-close");
+
 var delivery = document.querySelector(".delivery-service");
 var dslide = document.querySelector(".delivery-slide");
+
 var guarantee = document.querySelector(".guarantee-service");
 var gslide = document.querySelector(".guarantee-slide");
+
 var credit = document.querySelector(".credit-service");
 var cslide = document.querySelector(".credit-slide");
-var tr = popup.classList.contains(".fadeout-map");
+
 window.onload = function (event) {
 	delivery.classList.add("show-d");
 };
@@ -46,35 +52,91 @@ credit.addEventListener("click", function (event) {
 	delivery.classList.remove("show-d");
 });
 
- 
 
 showmap.addEventListener("click", function (event) {
 	event.preventDefault();
 	popup.classList.add("show-map");
+	popup.classList.remove("fadeout");
 });
-
 close.addEventListener("click", function (event) {
 	event.preventDefault();
 	popup.classList.remove("show-map");
-	
+	popup.classList.add("fadeout");
 });
+function closeMap() {
+	popup.classList.remove("fadeout");
+}
+popup.addEventListener("animationend", closeMap, false);
+
+window.addEventListener("keydown", function (event) {
+	if (event.keyCode === 27) {
+		if (popup.classList.contains("show-map"
+		         )) {
+		    popup.classList.remove("show-map");
+	    }
+    }
+});
+
 
 
 mail.addEventListener("click", function (event) {
 	event.preventDefault();
 	mailform.classList.add("show-mail");
+	mailform.classList.remove("fadeout");
 });
 closemail.addEventListener("click", function (event) {
 	event.preventDefault();
 	mailform.classList.remove("show-mail");
+	mailform.classList.add("fadeout");
 });
-for (var i = 0; i < buy.length; i++) {
+function closeMail() {
+	mailform.classList.remove("fadeout");
+}
+mailform.addEventListener("animationend", closeMail, false);
+
+window.addEventListener("keydown", function (event) {
+	if (event.keyCode === 27) {
+		if (mailform.classList.contains("show-mail"
+		         )) {
+		    mailform.classList.remove("show-mail");
+	    }
+    }
+});
+
+
+
+for (i = 0; i < buy.length; i++) {
 	buy[i].addEventListener('click', function (event) {
 		event.preventDefault();
 		buyform.classList.add("show-buy-form");
+		buyform.classList.remove("fadeout");
 	});
 }
 exitbuyform.addEventListener("click", function (event) {
 	event.preventDefault();
 	buyform.classList.remove("show-buy-form");
+	buyform.classList.add("fadeout");
 });
+
+function closeBuy() {
+	buyform.classList.remove("fadeout");
+}
+buyform.addEventListener("animationend", closeBuy, false);
+
+window.addEventListener("keydown", function (event) {
+	if (event.keyCode === 27) {
+		if (buyform.classList.contains("show-buy-form"
+		        )) {
+		    buyform.classList.remove("show-buy-form");
+	    }
+    }
+});
+
+
+
+
+
+
+
+
+
